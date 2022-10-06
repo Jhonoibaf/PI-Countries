0829786b9ea03c54d2c9cbe704e7c746472be114
johnoibaf <jroafernandez@gmail.com>
2022-10-06 16:27:17 -0500

    config entorno

--- api/index.js
+++ api/index.js
@@ -19,10 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const { SERVER_PORT} = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(SERVER_PORT, () => {
+    console.log('%s listening at '+ SERVER_PORT); // eslint-disable-line no-console
   });
 });
